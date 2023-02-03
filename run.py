7b291e7baefc9b164781c3623f2b953a09855ae1
digimori <hanaorikuu@gmail.com>
2023-02-03 13:17:25 +0000

    Additional validation added by iterating through the string provided

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def validate_data(values):
      or if there are not exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values must be entered, you providd {len(values)}"
